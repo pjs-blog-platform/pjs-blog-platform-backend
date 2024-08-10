85ebcb322a1b0dfbde15ab83a4eef2883b5480ce
Paulo Silva <pjorge.silvaa@gmail.com>
2024-08-10 20:53:12 +0100

    Creation of DB Migrations. Cleanup of code.

--- Users.API/Models/User.cs
+++ Users.API/Models/User.cs
@@ -7,9 +7,9 @@ namespace Users.API.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public string FullName { get; set; }
-        public string Bio { get; set; }
-        public string ProfilePictureUrl { get; set; }
+        public string? FullName { get; set; }
+        public string? Bio { get; set; }
+        public string? ProfilePictureUrl { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public bool IsActive { get; set; }
